e960cd79e00405a9f74f7b76c43c0e440f5247fd
MaksymSirenko <brute1988@gmail.com>
2024-07-23 22:36:34 +0200

    first commit

---
+++ src/test/java/com/tabakshop/tests/TestBase.java
@@ -0,0 +1,30 @@
+/*
+ * created by max$
+ */
+
+
+package com.tabakshop.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class TestBase {
+    public WebDriver driver;
+    @BeforeMethod
+    public void init() {
+        driver = new ChromeDriver();
+        driver.get("");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+
+    @AfterMethod(enabled = false)
+    public void tearDown() {
+        driver.quit();
+    }
+}
