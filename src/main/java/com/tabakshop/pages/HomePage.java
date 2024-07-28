dedac2445acae58fc4a580b16e914d14aefe4556
MaksymSirenko <brute1988@gmail.com>
2024-07-29 01:11:38 +0200

    registrationTests

---
+++ src/main/java/com/tabakshop/pages/HomePage.java
@@ -0,0 +1,24 @@
+/*
+ * created by $
+ */
+
+
+package com.tabakshop.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage {
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy()
+    WebElement registrationButton;
+
+    public RegistrationPage clickOnRegistrationButton() {
+        click(registrationButton);
+        return new RegistrationPage(driver);
+    }
+}
