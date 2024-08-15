24184dde5b4a86eee70c44c9c479043e26c4d3f0
IvannnnavI <io@email.>
2024-08-15 11:12:34 +0200

    AdminLogin

--- src/main/java/com/tabakshop/data/Data.java
+++ src/main/java/com/tabakshop/data/Data.java
@@ -16,12 +16,15 @@ public class Data {
     public static final String DO_NOT_MATCH_MESSAGE = "Passwords do not match";
     public static final String EMAIL_EXAMPLE = "test34@gmail.com";
     public static final String PASSWOR_EXAMPLE = "7465HHHY***$64";
+    public static final String EMAIL_ADMIN = "tobeornottobe@gmail.com";
+    public static final String PASSWOR_ADMIN = "myadminpasswordIsStrong***33";
     public static final String EXISTS_USER_MESSAGE = "User already exists. Please try a different email.";
     public static final String NOT_VALID_PASSWORD_MESSAGE = "Password must be in a valid format.";
     public static final String EMPTY = "";
 
     public static final String LOGIN_MESSAGE = "Login";
     public static final String LOGOUT_BUTTON  = "Logout";
+    public static final String ADMIN_GREETING  = "Hello, Admin :)";
     public static final String NOT_EXIST_PASSWORD = "7465HffHHY***$64";
     public static final String WRONG_EMAIL_EXAMPLE = "test3@gmail.com";
 
